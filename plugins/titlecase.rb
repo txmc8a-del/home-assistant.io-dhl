028f044d789eb81ef47695ae5886e222019da09b
txmc8a-del <txmc8a@gmail.com>
2026-04-18 10:07:29 -0700

    Update titlecase.rb

--- plugins/titlecase.rb
+++ plugins/titlecase.rb
@@ -1,36 +1,28 @@
-class String
-  def titlecase
-    small_words = %w(a an and as at but by en for if in of on or the to v v. via vs vs.)
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import java.awt.print.PrinterJob;
 
-    x = split(" ").map do |word|
-      # note: word could contain non-word characters!
-      # downcase all small_words, capitalize the rest
-      small_words.include?(word.gsub(/\W/, "").downcase) ? word.downcase! : word.smart_capitalize!
-      word
-    end
-    # capitalize first and last words
-    x.first.to_s.smart_capitalize!
-    x.last.to_s.smart_capitalize!
-    # small words are capitalized after colon, period, exclamation mark, question mark
-    x.join(" ").gsub(/(:|\.|!|\?)\s?(\W*#{small_words.join("|")}\W*)\s/) { "#{$1} #{$2.smart_capitalize} " }
-  end
+// 1. Find all available printers
+PrintService[] services = PrintServiceLookup.lookupPrintServices(null, null);
+PrintService myPrinter = null;
 
-  def titlecase!
-    replace(titlecase)
-  end
+// 2. Find the one matching your network IP or name
+for (PrintService service : services) {
+    if (service.getName().contains("192.168.2.20")) {
+        myPrinter = service;
+        break;
+    }
+}
 
-  def smart_capitalize
-    # ignore any leading crazy characters and capitalize the first real character
-    if self =~ /^['"\(\[']*([a-z])/
-      i = index($1)
-      x = self[i,self.length]
-      # word with capitals and periods mid-word are left alone
-      self[i,1] = self[i,1].upcase unless x =~ /[A-Z]/ or x =~ /\.\w+/
-    end
-    self
-  end
-
-  def smart_capitalize!
-    replace(smart_capitalize)
-  end
-end
+if (myPrinter != null) {
+    PrinterJob job = PrinterJob.getPrinterJob();
+    job.setPrintService(myPrinter); // Target the specific printer
+    try {
+        job.print();
+        System.out.println("Print job sent to 192.168.2.20");
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+} else {
+    System.out.println("Printer at 192.168.2.20 not found. Check network connection.");
+}
